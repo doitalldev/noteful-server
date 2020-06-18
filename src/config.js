d373c20394f6cca96c59dd7fd9293dd189b27de3
Jonathan Clark <jonclark822@gmail.com>
2020-06-18 16:36:57 -0700

    pushing to heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+  DATABASEB_URL:
+    process.env.DATABASEB_URL ||
+    'postgresql://dunder_mifflin@localhost/blogful',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://dunder_mifflin@localhost/blogful-test',
 };
